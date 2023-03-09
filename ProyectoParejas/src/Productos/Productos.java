bad038ad964f5c82cd8062fcf6ace398eb4410e2
Zethuwu <oscardelvillar39@aragon.unam.mx>
2023-03-08 18:59:06 -0600

    carpeta productos

---
+++ ProyectoParejas/src/Productos/Productos.java
@@ -0,0 +1,38 @@
+package Productos;
+
+public class Productos {
+
+    private String nombreProducto;
+    private int precio;
+    private String codigo;
+
+    public Productos(String nombreProducto,int precio, String codigo) {
+        this.nombreProducto = nombreProducto;
+        this.precio = precio;
+        this.codigo = codigo;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+}
