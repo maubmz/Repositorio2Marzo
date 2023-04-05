5f09307cf513b3e51f6938999acfae40f2f6c90a
Alexis <alexislombardini41@aragon.unam.mx>
2023-04-05 17:55:10 -0600

    Put/Patch/Delete

--- Tienda/src/main/java/mx/com/digitalchallengers/controlador/ClienteCotrolador.java
+++ Tienda/src/main/java/mx/com/digitalchallengers/controlador/ClienteCotrolador.java
@@ -3,11 +3,13 @@ package mx.com.digitalchallengers.controlador;
 import mx.com.digitalchallengers.entidades.Cliente;
 import mx.com.digitalchallengers.repositorios.ClienteRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.persistence.ManyToMany;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,6 +40,29 @@ public class ClienteCotrolador {
         return clienteRepositorio.findById(id);
     }
 
+    @GetMapping("/findByName")
+    public List<Cliente> findByName(@RequestParam String nombre){
+        return clienteRepositorio.findByNombreContaining(nombre);
+    }
 
+    @DeleteMapping(path = "/{id}")
+    public void deleteById(@PathVariable int id){
+        clienteRepositorio.deleteById(id);
+    }
 
+    @PutMapping(path = "/{id}")
+    public void updateById(@PathVariable int id, @RequestBody Cliente clienteDos){
+        Cliente cliente= clienteRepositorio.findById(id).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "no se encontro cliente"));
+        cliente.setNombre(clienteDos.getNombre());
+        cliente.setApellido(clienteDos.getApellido());
+        cliente.setCorreo(clienteDos.getCorreo());
+        clienteRepositorio.save(cliente);
+    }
+
+    @PatchMapping(path = "/{id}")
+    public Cliente actualizarNombreById(@PathVariable int id, @RequestBody Cliente clientePatch){
+        Cliente cliente=clienteRepositorio.findById(id).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "no se encontro cliente"));
+        cliente.setNombre(clientePatch.getNombre());
+        return clienteRepositorio.save(cliente);
+    }
 }
--- Tienda/src/main/java/mx/com/digitalchallengers/repositorios/ClienteRepositorio.java
+++ Tienda/src/main/java/mx/com/digitalchallengers/repositorios/ClienteRepositorio.java
@@ -4,8 +4,10 @@ import mx.com.digitalchallengers.entidades.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClienteRepositorio extends JpaRepository<Cliente,Integer> {
-
+    List<Cliente> findByNombreContaining(String nombre);
 
 }
